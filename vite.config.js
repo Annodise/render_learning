dcdd827dec2a58785f18a8ec52b2cae3409dfc18
Annodise <Annodise@gmail.com>
2025-11-09 00:41:32 +0700

    -

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import { resolve } from "path"
+
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  root: ".",
+  plugins: [react()],
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/main_page/index.html"), // ✅ หน้าแรกของเว็บ
+      },
+    },
+  },
+
+
+  server: {
+  port: 5173,
+  open: '/src/main_page/index.html', // ✅ เวลา dev ให้เปิดหน้านี้โดยอัตโนมัติ
+  },
+
+
+});
